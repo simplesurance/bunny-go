d9e5f0a69d6a3f3fb8f37e42141f15a1ad85f782
Fabian Holler <fabian.holler@simplesurance.de>
2021-11-01 14:53:51 +0100

    edgerule: make the implementation more similar to the API
    - define an own message struct for the AddOrUpdateEdgeRule method, instead of
      referencing the EdgeRule struct that is embedded in the PullZone message
      returned by the Get Endpoint.
      This ensures that the messages can be updated separately in the future without
      breaking the package API.
    - Pass a SetEdgeRuleEnabledOptions struct to SetEdgeRuleEnabled instead of
      passing only the enabled bool as method parameter.
      This is more close to the API and it will allow to extend the information that
      are sent to the API endpoint without breaking the package API.
      Which value is expected as SetEdgeRuleEnabledOptions.ID is documented in the
      godoc. Additionally a warning message is logged if the passed pull zone ID
      that is used as part of the request URL is different then the one set in
      SetSetEdgeRuleEnabled.ID.

--- pullzone_edgerule_delete.go
+++ pullzone_edgerule_delete.go
@@ -6,6 +6,8 @@ import (
 )
 
 // DeleteEdgeRule removes an Edge Rule of a Pull Zone.
+// The edgeRuleGUID field is called edgeRuleID in the API message and
+// documentation. It is the same then the GUID field in the EdgeRule message.
 //
 // Bunny.net API docs: https://docs.bunny.net/reference/pullzonepublic_deleteedgerule
 func (s *PullZoneService) DeleteEdgeRule(ctx context.Context, pullZoneID int64, edgeRuleGUID string) error {
--- pullzone_edgerule_set_enabled.go
+++ pullzone_edgerule_set_enabled.go
@@ -5,25 +5,30 @@ import (
 	"fmt"
 )
 
-// edgeRuleEnableOptions represents the message that is sent to Add/Update Edge Rule endpoint.
+// SetEdgeRuleEnabledOptions represents the message that is sent to Add/Update Edge Rule endpoint.
 //
 // Bunny.net API docs: https://docs.bunny.net/reference/pullzonepublic_addedgerule
-type edgeRuleEnableOptions struct {
-	// ID is the pull zone id
+type SetEdgeRuleEnabledOptions struct {
+	// ID must be set to the PullZone ID for that the EdgeRule should be enabled.
 	ID    *int64 `json:"Id,omitempty"`
 	Value *bool  `json:"Value,omitempty"`
 }
 
 // SetEdgeRuleEnabled enables or disables an Edge Rule of a Pull Zone.
+// The edgeRuleGUID field is called edgeRuleID in the API message and
+// documentation. It is the same then the GUID field in the EdgeRule message.
 //
 // Bunny.net API docs: https://docs.bunny.net/reference/pullzonepublic_addedgerule
-func (s *PullZoneService) SetEdgeRuleEnabled(ctx context.Context, pullZoneID int64, edgeRuleGUID string, enabled bool) error {
-	msg := edgeRuleEnableOptions{
-		ID:    &pullZoneID,
-		Value: &enabled,
+func (s *PullZoneService) SetEdgeRuleEnabled(ctx context.Context, pullZoneID int64, edgeRuleGUID string, opts *SetEdgeRuleEnabledOptions) error {
+	if opts != nil {
+		if opts.ID == nil {
+			s.client.logf("SetEdgeRuleEnabled: ID field is unset in SetEdgeRuleEnabledOptions")
+		} else if *opts.ID != pullZoneID {
+			s.client.logf("SetEdgeRuleEnabled: mismatched pullZoneID %d and SetEdgeRuleEnabledOptions.ID %d were passed, values should be equal", pullZoneID, *opts.ID)
+		}
 	}
 
-	req, err := s.client.newPostRequest(fmt.Sprintf("pullzone/%d/edgerules/%s/setEdgeRuleEnabled", pullZoneID, edgeRuleGUID), &msg)
+	req, err := s.client.newPostRequest(fmt.Sprintf("pullzone/%d/edgerules/%s/setEdgeRuleEnabled", pullZoneID, edgeRuleGUID), opts)
 	if err != nil {
 		return err
 	}
