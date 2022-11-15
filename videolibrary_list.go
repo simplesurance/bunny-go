262b7c4d287efb058ac1e578680190ff4ec919f0
Fabian Holler <fabian.holler@simplesurance.de>
2022-11-15 12:02:51 +0100

    videolibrary/list: simplify the implementation
    Merge the videLibraryList() function into the VideoLibraryService.List() method.

--- videolibrary_list.go
+++ videolibrary_list.go
@@ -23,20 +23,13 @@ func (s *VideoLibraryService) List(
 	ctx context.Context,
 	opts *VideoLibraryListOpts,
 ) (*VideoLibraries, error) {
-	return videLibraryList[VideoLibraries](ctx, s.client, "/videolibrary", opts)
-}
+	const path = "/videolibrary"
+	var res VideoLibraries
 
-// NOTE: this is an override of resourceList strictly for the purpose of
-// providing the extra query param options in VideoLibraryGetOpts. In the future
-// hopefully it can be removed for a better solution. See the following discussion:
-// https://github.com/simplesurance/bunny-go/pull/27#discussion_r1021270152
-func videLibraryList[Resp any](
-	ctx context.Context,
-	client *Client,
-	path string,
-	opts *VideoLibraryListOpts,
-) (*Resp, error) {
-	var res Resp
+	// NOTE: The resourceList function is not used for the purpose of
+	// providing the extra query param options in VideoLibraryGetOpts. In the future
+	// hopefully it can be removed for a better solution. See the following discussion:
+	// https://github.com/simplesurance/bunny-go/pull/27#discussion_r1021270152
 
 	// Ensure that opts.Page is >=1, if it isn't bunny.net will send a
 	// different response JSON object, that contains only a single Object,
@@ -53,12 +46,12 @@ func videLibraryList[Resp any](
 		opts.ensureConstraints()
 	}
 
-	req, err := client.newGetRequest(path, opts)
+	req, err := s.client.newGetRequest(path, opts)
 	if err != nil {
 		return nil, err
 	}
 
-	if err := client.sendRequest(ctx, req, &res); err != nil {
+	if err := s.client.sendRequest(ctx, req, &res); err != nil {
 		return nil, err
 	}
 
